cf4ab941340e688a9bef69d7e44c37f9172310db
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-05-26 12:24:08 +0900

    Departmentリポジトリのテストまで完了

--- test/Infrastructures/Repositories/DepartmentRepositoryTests.cs
+++ test/Infrastructures/Repositories/DepartmentRepositoryTests.cs
@@ -1,21 +1,17 @@
-using System.Collections;
-using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using csharp_training_202605.Applications.Domains;
-using csharp_training_202605.Exceptions;
 using csharp_training_202605.Infrastructures.Adapters;
 using csharp_training_202605.Infrastructures.Context;
-using csharp_training_202605.Infrastructures.Entities;
 using csharp_training_202605.Infrastructures.Repositories;
-using csharp_training_202605.tests.TestDoubles;
+using csharp_training_202605.Exceptions;
 
-namespace csharp_training_202605.Tests.Infrastructures.Repositories;
+namespace csharp_training_202605.Test.Infrastructures.Repositories;
 
 [DoNotParallelize]
 [TestClass]
-public class DepartmentRepositoryTests
+public class RightExistTest
 {
     private const string ConnectionString =
         "Host=localhost;Port=5432;Database=csharp_training_202605;Username=postgres;Password=training;";
@@ -23,10 +19,159 @@ public class DepartmentRepositoryTests
     private DepartmentRepository _departmentrepository = null!;
     private AppDbContext _context = null!;
 
-    [TestMethod]
-    public void FindAll_NoDapartments_exist()
+    [TestInitialize]
+    public void Setup()
     {
-        var adapter = new DepartmentEntityAdapter();
+        var departmentAdapter = new DepartmentEntityAdapter();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(ConnectionString)
+            .Options;
+
+        _context = new AppDbContext(options);
+
+        var path = Path.Combine(AppContext.BaseDirectory, "sql", "exist.sql");
+        var sql = File.ReadAllText(path);
+        _context.Database.ExecuteSqlRaw(sql);
+
+        _departmentrepository = new DepartmentRepository(_context, departmentAdapter);
+    }
+
+    private static void AssertDepartment(Department department, int id, string name)
+    {
+        Assert.AreEqual(id, department.Id);
+        Assert.AreEqual(name, department.Name);
+    }
+
+    [TestMethod]
+    public void Department_FindAll_ReturnsAllDepartments()
+    {
+        var departments = _departmentrepository.FindAll();
+        Assert.AreEqual(3, departments.Count);
+        AssertDepartment(departments[0], 1, "総務部");
+        AssertDepartment(departments[1], 2, "経理部");
+        AssertDepartment(departments[2], 3, "開発部");
+    }
+
+    [TestMethod]
+    public void Department_FindById_ReturnsDepartment()
+    {
+        var department = _departmentrepository.FindById(1);
+        AssertDepartment(department!, 1, "総務部");
+    }
+
+    [TestMethod]
+    public void Department_FindById_ReturnsNull()
+    {
+        var department = _departmentrepository.FindById(5);
+        Assert.IsNull(department);
+    }
+
+    [TestMethod]
+    public void Department_Create_Added()
+    {
+        var addentity = new Department("営業部"); //追加するエンティティ
+        _departmentrepository.Create(addentity);
+
+        var departments = _departmentrepository.FindAll();//DBから追加後の従業員リストを取得
+
+        Assert.AreEqual(4, departments.Count);
+        AssertDepartment(departments[0], 1, "総務部");
+        AssertDepartment(departments[1], 2, "経理部");
+        AssertDepartment(departments[2], 3, "開発部");
+        AssertDepartment(departments[3], 4, "営業部");
+    }
+
+    [TestMethod]
+    public void Department_Delete_Deleted()
+    {
+        var deleteentity = new Department(3, "開発部"); //追加するエンティティ
+        _departmentrepository.Delete(deleteentity);
+
+        var departments = _departmentrepository.FindAll();//DBから追加後の従業員リストを取得
+
+        Assert.AreEqual(2, departments.Count);
+        AssertDepartment(departments[0], 1, "総務部");
+        AssertDepartment(departments[1], 2, "経理部");
+    }
+}
+
+[DoNotParallelize]
+[TestClass]
+public class ExceptionsTest
+{
+    private const string ConnectionString =
+        "Host=localhost;Port=5432;Database=csharp_training_202605;Username=postgres;Password=training;";
+
+    private DepartmentRepository _departmentrepository = null!;
+    private AppDbContext _context = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var departmentAdapter = new DepartmentEntityAdapter();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .Options;
+
+        _context = new AppDbContext(options);
+
+        _departmentrepository = new DepartmentRepository(_context, departmentAdapter);
+    }
+
+    private static void AssertDepartment(Department department, int id, string name)
+    {
+        Assert.AreEqual(id, department.Id);
+        Assert.AreEqual(name, department.Name);
+    }
+
+    [TestMethod]
+    public void Department_FindAll_Exception()
+    {
+        var exception = Assert.ThrowsException<InternalException>(() => _departmentrepository.FindAll());
+
+        Assert.IsInstanceOfType<InvalidOperationException>(exception.InnerException);
+    }
+
+    [TestMethod]
+    public void Department_FindById_Exception()
+    {
+        var exception = Assert.ThrowsException<InternalException>(() => _departmentrepository.FindById(1));
+
+        Assert.IsInstanceOfType<InvalidOperationException>(exception.InnerException);
+    }
+
+    [TestMethod]
+    public void Department_Create_Exception()
+    {
+        var addentity = new Department("開発部");
+        var exception = Assert.ThrowsException<InternalException>(() => _departmentrepository.Create(addentity));
+
+        Assert.IsInstanceOfType<InvalidOperationException>(exception.InnerException);
+    }
+
+    [TestMethod]
+    public void Department_Delete_Exception()
+    {
+        var deleteentity = new Department(1, "総務部");
+        var exception = Assert.ThrowsException<InternalException>(() => _departmentrepository.Create(deleteentity));
+
+        Assert.IsInstanceOfType<InvalidOperationException>(exception.InnerException);
+    }
+}
+
+[DoNotParallelize]
+[TestClass]
+public class RightNoneTest
+{
+    private const string ConnectionString =
+        "Host=localhost;Port=5432;Database=csharp_training_202605;Username=postgres;Password=training;";
+
+    private DepartmentRepository _departmentrepository = null!;
+    private AppDbContext _context = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var departmentAdapter = new DepartmentEntityAdapter();
         var options = new DbContextOptionsBuilder<AppDbContext>()
             .UseNpgsql(ConnectionString)
             .Options;
@@ -37,68 +182,13 @@ public class DepartmentRepositoryTests
         var sql = File.ReadAllText(path);
         _context.Database.ExecuteSqlRaw(sql);
 
-        _departmentrepository = new DepartmentRepository(_context, adapter);
+        _departmentrepository = new DepartmentRepository(_context, departmentAdapter);
+    }
 
+    [TestMethod]
+    public void Department_FindAll_ReturnsZeroDepartment()
+    {
         var departments = _departmentrepository.FindAll();
         Assert.AreEqual(0, departments.Count);
     }
-
-    [TestMethod]
-    public void FindAll_ReturnsAllDepartmentsNew()
-    {
-        var adapter = new DepartmentEntityAdapter();
-        var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseNpgsql(ConnectionString)
-            .Options;
-
-        _context = new AppDbContext(options);
-
-        var path = Path.Combine(AppContext.BaseDirectory, "sql", "exist.sql");
-        var sql = File.ReadAllText(path);
-        _context.Database.ExecuteSqlRaw(sql);
-
-        _departmentrepository = new DepartmentRepository(_context, adapter);
-
-        var departments = _departmentrepository.FindAll();
-
-        Assert.AreEqual(2, departments.Count);
-        AssertDepartment(departments[0], 1, "総務部");
-        AssertDepartment(departments[1], 2, "経理部");
-    }
-
-    [TestMethod]
-    public void FindAll_WhenDbSetThrows_WrapsExceptionInInternalException()
-    {
-        var adapter = new DepartmentEntityAdapter();
-        var options = new DbContextOptionsBuilder<AppDbContext>()
-            .Options;
-
-        _context = new AppDbContext(options);
-
-        var path = Path.Combine(AppContext.BaseDirectory, "sql", "exist.sql");
-        var sql = File.ReadAllText(path);
-        //_context.Database.ExecuteSqlRaw(sql);
-
-        _departmentrepository = new DepartmentRepository(_context, adapter);
-
-        var exception = Assert.ThrowsException<InternalException>(() => _departmentrepository.FindAll());
-
-        Assert.IsInstanceOfType<InvalidOperationException>(exception.InnerException);
-    }
-
-    private static void AssertDepartment(Department department, int id, string name)
-    {
-        Assert.AreEqual(id, department.Id);
-        Assert.AreEqual(name, department.Name);
-    }
-
-    private static AppDbContext CreateContext(DbSet<DepartmentEntity> departments)
-    {
-        var options = new DbContextOptionsBuilder<AppDbContext>().Options;
-        return new TestAppDbContext(options)
-        {
-            Departments = departments,
-        };
-    }
-
 }
--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0166c0e166a3eb96cb455fbb8ce3941e811776fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b266d62d5cd7d86b93e948e5a5c3948ad7a107")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
