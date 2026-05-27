909f418d9b6b03a9d74ca8eb950b692a9fd66631
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-05-27 16:28:46 +0900

    0527

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d128aef81997489932c3ff47f3d673f2dc1c4c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb4fcc1233fc965012750d6339370d1fcfb4818")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
