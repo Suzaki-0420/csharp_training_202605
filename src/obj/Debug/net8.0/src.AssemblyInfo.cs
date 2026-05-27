5a7c2a74c15bd9a6e66d342484872548be250d31
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-05-27 13:51:51 +0900

    ブラックボックステスト修正

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515e56b65852e17ff2103beb29431c38dd9a4eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd957b72054beb15b154a0182d10acd9b6d72508")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
