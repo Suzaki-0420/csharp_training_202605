7d79c36bbdd6466ebbe1ee5b774d291e74d60453
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-05-28 16:20:00 +0900

    資料修正

--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2395120d91e7a98948c74d286eed951f07eba99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7883f0e39005787ec511d6f3662a2996a3ac00")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
